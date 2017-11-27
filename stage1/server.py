4eff830549c4db45854feac4f03aa434b172ea2a
Gowda <magowda@corp.siriusxm.com>
2017-11-27 10:14:29 -0500

    Added more details

--- stage1/server.py
+++ stage1/server.py
@@ -2,6 +2,13 @@
 from socket import *
 
 
+def fib(n: int):
+    if (n < 2):
+        return n
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+
 def fib_server(address):
     sock = socket()
     sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
@@ -11,3 +18,17 @@ def fib_server(address):
         client, addr = sock.accept()
         print("Connection ==>", addr)
 
+
+def fib_handler(client: socket):
+    while True:
+        req = client.recv(100)
+        if not req:
+            break
+        n = int(req)
+        result = fib(n)
+        resp = str(result).encode('ascii') + b'\n'
+        client.send(resp)
+    print('closed')
+
+
+fib_server(('', 25000))
